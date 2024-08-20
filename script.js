e6853683d9f4ac8f59e18f8d8b213cd47381f015
Audipunk <audipunkwp@gmail.com>
2024-08-20 19:11:05 +0200

    update

--- script.js
+++ script.js
@@ -5,8 +5,7 @@ function validateSyntax() {
     const regex = /^pet_\d{4}[^\d_]+$/; // RegEx to validate the input
 
   const resultElement = document.getElementById("result");
-}
-{
+
   // Validation logic
   result = regex.test(input) ? "Valid Syntax" : "Invalid Syntax";
 
